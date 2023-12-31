42a1670fac441cca37ec56e5585aec5f9e04f167
M1HA1 <67576278+M1HA15@users.noreply.github.com>
2023-12-31 10:28:19 +0200

    Update NSR.bat

--- NSR.bat
+++ NSR.bat
@@ -25,9 +25,10 @@ for /f "delims=" %%V in ('powershell -Command "$PSVersionTable.PSVersion.Major.T
 )
 
 echo ---------------------------------------------------------------------
-echo                               NSR (0.3)
+echo                               NSR (0.4)
 echo.
 echo     Warning: The script is running with administrator privileges!
+echo           Warning: This version may contain bugs or issues!
 echo                    PowerShell Version: %PSVersion%
 echo ---------------------------------------------------------------------
 echo.
@@ -53,91 +54,134 @@ echo.
 echo [5] Resetting Winsock...
 netsh winsock reset
 echo.
-echo.
+
+timeout /nobreak /t 1 > nul
 
 echo [6] Set DNS provider:
+echo     [1] Recommended DNS Providers
+echo     [2] Input your DNS Providers
+echo     [3] Skip
+echo.
+
+set /p "dnsChoice=Choose DNS provider (1-3): "
+echo.
+
+if "%dnsChoice%"=="" goto :eof
+
+if "%dnsChoice%"=="1" goto :recommendedDNS
+if "%dnsChoice%"=="2" goto :customDNS
+if "%dnsChoice%"=="3" goto :skipDNS
+
+echo Invalid choice. Please select a valid option.
+goto :eof
+
+:recommendedDNS
+echo Testing DNS providers to find the best one for you...
+for %%i in (1.1.1.1 208.67.222.222 95.85.95.85 9.9.9.9 8.8.8.8) do (
+    for /f "tokens=*" %%a in ('ping -n 4 -w 1000 %%i ^| find "Minimum"') do (
+        echo %%i: %%a
+    )
+)
+echo.
+echo Testing complete! The DNS provider with the lowest ping times may be the best choice for you.
+echo.
+echo.
+echo Choose a recommended DNS provider:
 echo     [1] Cloudflare (1.1.1.1)
 echo     [2] Cisco Umbrella (208.67.222.222)
 echo     [3] GCore (95.85.95.85)
 echo     [4] Quad9 (9.9.9.9)
 echo     [5] Google (8.8.8.8)
 echo.
-
-set /p "dnsChoice=Choose DNS provider (1-5, press Enter to skip): "
-echo.
-if not defined dnsChoice goto :skipDNS
-
-if "%dnsChoice%"=="1" (
+set /p "recommendedDNSChoice=Enter your choice (1-5): "
+if "%recommendedDNSChoice%"=="1" (
     set DNS_IP=1.1.1.1
     set DNS_Secondary_IP=1.0.0.1
-    set DNS_IPv6=2606:4700:4700::1111
+	set DNS_IPv6=2606:4700:4700::1111
     set DNS_Secondary_IPv6=2606:4700:4700::1001
-) else if "%dnsChoice%"=="2" (
+) else if "%recommendedDNSChoice%"=="2" (
     set DNS_IP=208.67.222.222
     set DNS_Secondary_IP=208.67.220.220
-    set DNS_IPv6=2620:119:35::35
+	set DNS_IPv6=2620:119:35::35
     set DNS_Secondary_IPv6=2620:119:53::53
-) else if "%dnsChoice%"=="3" (
+) else if "%recommendedDNSChoice%"=="3" (
     set DNS_IP=95.85.95.85
     set DNS_Secondary_IP=2.56.220.2
-    set DNS_IPv6=2a03:90c0:999d::1
+	set DNS_IPv6=2a03:90c0:999d::1
     set DNS_Secondary_IPv6=2a03:90c0:9992::1
-) else if "%dnsChoice%"=="4" (
+) else if "%recommendedDNSChoice%"=="4" (
     set DNS_IP=9.9.9.9
     set DNS_Secondary_IP=149.112.112.112
-    set DNS_IPv6=2620:fe::fe
+	set DNS_IPv6=2620:fe::fe
     set DNS_Secondary_IPv6=2620:fe::9
-) else if "%dnsChoice%"=="5" (
+) else if "%recommendedDNSChoice%"=="5" (
     set DNS_IP=8.8.8.8
     set DNS_Secondary_IP=8.8.4.4
-    set DNS_IPv6=2001:4860:4860::8888
+	set DNS_IPv6=2001:4860:4860::8888
     set DNS_Secondary_IPv6=2001:4860:4860::8844
 ) else (
-    echo Skipping DNS provider selection due to the selected option being nonexistent..
-    echo If you wish to choose a DNS provider, you can run the script again!
-    echo.
-    goto :skipDNS
+	echo.
+    echo You have chosen an invalid option! Please select a correct option...
+    goto :recommendedDNS
+	echo.
 )
+goto :setDNS
 
-if defined DNS_IP (
+:customDNS
+set /p "visitDNSPerf=Do you want to visit DNSPerf to choose a DNS provider? (Y/N): "
+if /i "%visitDNSPerf%"=="Y" (
+    start "" "https://www.dnsperf.com/"
+)
+echo.
+echo Please input the IPv4 and IPv6 addresses of the selected DNS provider!
+set /p "DNS_IP=Enter your primary IPv4 DNS address: "
+set /p "DNS_Secondary_IP=Enter your secondary IPv4 DNS address: "
+set /p "DNS_IPv6=Enter your primary IPv6 DNS address: "
+set /p "DNS_Secondary_IPv6=Enter your secondary IPv6 DNS address: "
+echo Please wait a moment while we configure the desired addresses for the selected DNS provider.
+echo.
+goto :setDNS
+
+:skipDNS
+echo Skipping DNS provider selection...
+echo.
+goto :setGitHub
+
+:setDNS
+if not "%DNS_IP%"=="" (
     powershell -Command "& { Get-NetAdapter | ForEach-Object { try { Set-DnsClientServerAddress -InterfaceAlias $_.Name -ServerAddresses @('%DNS_IP%', '%DNS_Secondary_IP%') -ErrorAction Stop } catch { Write-Error $_ } } }"
     echo Primary IPv4 DNS set to %DNS_IP%
     echo Secondary IPv4 DNS set to %DNS_Secondary_IP%
-	echo Your selected IPv4 DNS provider is now active!
 )
-
-echo.
-
-if defined DNS_IPv6 (
+if not "%DNS_IPv6%"=="" (
     powershell -Command "& { Get-NetAdapter | ForEach-Object { try { Set-DnsClientServerAddress -InterfaceAlias $_.Name -ServerAddresses @('%DNS_IPv6%', '%DNS_Secondary_IPv6%') -ErrorAction Stop } catch { Write-Error $_ } } }"
     echo Primary IPv6 DNS set to %DNS_IPv6%
     echo Secondary IPv6 DNS set to %DNS_Secondary_IPv6%
-	echo Your selected IPv6 DNS provider is now active!
 )
-
 echo.
 
-:skipDNS
+:setGitHub
 set /p "skipGitHub=Do you want to open the GitHub page of this project? (Y/N): "
 if /i "%skipGitHub%"=="Y" (
     echo Opening default web browser...
     start "" "https://github.com/M1HA15/Network-Settings-Reset"
 )
-
 echo.
 
+:setRestart
 set /p "skipRestartChoice=Do you want to restart the computer now? (Y/N): "
 if /i "%skipRestartChoice%"=="Y" (
-	echo We appreciate you using the script. Your computer will restart shortly!
+    echo We appreciate you using the script. Your computer will restart shortly!
     shutdown /r /t 5 /f
 ) else if /i "%skipRestartChoice%"=="N" (
-	echo We appreciate you using the script. Don't forget to restart your computer later!
+    echo We appreciate you using the script. Don't forget to restart your computer later!
     echo Waiting for 5 seconds before closing the window...
     timeout /nobreak /t 4 > nul
     exit
 ) else (
-    echo Invalid choice. Press any key to close the window...
-    pause > nul
+	echo You have chosen an invalid option! Please select a correct option...
+	echo.
+	goto :setRestart
 )
 
 :eof
